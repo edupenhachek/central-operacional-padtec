fde1c3b61f67ef6a507b9769f9f1df8371c1a86c
Skip <skip@usecurling.com>
2026-07-27 16:16:02 -0300

    Fix ProtectedRoute rendering error

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react'
-import { Navigate } from 'react'
+import { Navigate } from 'react-router-dom'
 import { useAuth } from '@/hooks/use-auth'
 
 interface ProtectedRouteProps {
