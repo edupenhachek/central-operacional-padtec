46cea2dfa295719adc39f665724633788cae2a28
Skip <skip@usecurling.com>
2026-07-27 16:21:32 -0300

    Fix: Correct useLocation import in Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Link, Outlet, useLocation, useNavigate } from 'react'
+import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom'
 import {
   LayoutDashboard,
   CheckSquare,
